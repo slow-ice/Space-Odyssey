2abec66f567934beda5926034992d195c47b88ce
slow_ice <1357449044@qq.com>
2023-10-28 17:40:24 +0800

    穿屏效果, 吸收资源

---
+++ Assets/Scripts/Others/NormalResource.cs
@@ -0,0 +1,32 @@
+﻿
+
+using Assets.Scripts.Utility.Pool;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Others {
+    public class NormalResource : Resources {
+        public ObjectPool parentPool;
+
+        public float fadeDistance = 0.5f;
+        public float moveSpeed = 1f;
+
+        public override void OnAbsorbAction(Transform playerTrans) {
+            if (!gameObject.activeSelf)
+                return;
+            StartCoroutine(MoveToCenter(playerTrans));
+        }
+
+        IEnumerator MoveToCenter(Transform center) {
+            while (Vector2.Distance(transform.position, center.position) > fadeDistance) {
+                transform.position = Vector2.Lerp(transform.position, center.position,
+                    moveSpeed * Time.fixedDeltaTime);
+                yield return null;
+            }
+            Debug.Log("fade");
+            parentPool.Recycle(gameObject, () => {
+                StopAllCoroutines();
+            });
+        }
+    }
+}
--- Assets/Scripts/Others/Resources.cs
+++ Assets/Scripts/Others/Resources.cs
@@ -4,14 +4,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // 可被吸收的资源
-public class Resources : MonoBehaviour, IAbsorb
-{
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CircleCollider2D))]
+public abstract class Resources : MonoBehaviour, IAbsorb {
     // 该资源的energy
     [SerializeField] int energy;
+
+    protected virtual void Awake() {
+        GetComponent<Collider2D>().isTrigger = true;
+    }
 
     public int GetEnergy()
     {
         Debug.Log("吸收能量");
         return energy;
-    }    
+    }
+
+    public virtual void OnAbsorbAction(Transform playerTrans) {
+        
+    }
 }
